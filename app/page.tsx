7b7993ef8b84043adaa96b13cdbd8009b930dd32
Sebas Dev <sebasariasdev@gmail.com>
2023-07-07 19:18:31 +0800

    Redirect to login if not authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
-export default function Home() {
-  return (
-    <main>
-      Whiteboard
-    </main>
-  )
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    redirect('/login');
+  }
+
+  return <main>Whiteboard</main>;
 }
