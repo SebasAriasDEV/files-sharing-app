58ee399a7cefdf1ea69be4e08ace8fe11a919ef3
Sebas Dev <sebasariasdev@gmail.com>
2023-07-08 20:53:11 +0800

    Upload file selector implemented

--- app/components/UploadFile.tsx
+++ app/components/UploadFile.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { ChangeEventHandler, useState } from 'react';
 
 type Props = {
   userId: string;
@@ -7,23 +8,41 @@ type Props = {
 
 export default function UploadFile({ userId }: Props) {
   const supabase = createClientComponentClient();
+  const [file, setFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const uploadFile = async () => {
-    const date = new Date().toLocaleString().replace(/[:, /]/g, '-');
-    const { data, error } = await supabase.storage
-      .from('files')
-      .upload(`${userId}/hola${date}.txt`, 'hola hola hola hola');
-    if (error) {
-      console.log(error);
+  const selectFile: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.files) {
+      setFile(e.target.files.length === 0 ? null : e.target.files[0]);
     }
   };
 
+  const uploadFile = async () => {
+    setIsLoading(true);
+
+    if (!file) return;
+    const date = new Date().toLocaleString().replace(/[:, /]/g, '-');
+    const { data, error } = await supabase.storage
+      .from('files')
+      .upload(`${userId}/${file.name}-${date}`, file);
+    if (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
-    <button
-      onClick={uploadFile}
-      className='bg-slate-800 text-white hover:bg-slate-700 py-2 px-3 rounded-md'
-    >
-      Upload file
-    </button>
+    <div className='flex flex-row items-center border p-4 gap-2'>
+      <span>{file?.name}</span>
+      <input type='file' className='flex-1' onChange={selectFile} />
+      <button
+        onClick={uploadFile}
+        className='bg-slate-800 text-white hover:bg-slate-700 py-2 px-3 rounded-md disabled:bg-slate-400'
+        disabled={file === null || isLoading}
+      >
+        Upload file
+      </button>
+    </div>
   );
 }
